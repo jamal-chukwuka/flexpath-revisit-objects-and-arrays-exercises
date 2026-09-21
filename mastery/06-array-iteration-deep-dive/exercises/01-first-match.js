7da452769913f3c121f4f229a10d7202ef5a3449
Jamal Nezianya <jnezia1@hotmail.com>
2026-09-21 09:14:21 -0500

    Add Array Iteration Deep Dive mastery checkpoint

---
+++ mastery/06-array-iteration-deep-dive/exercises/01-first-match.js
@@ -0,0 +1,8 @@
+/**
+ * Return the first ticket whose priority is "urgent" AND whose status is "open".
+ * Return undefined when none qualifies.
+ * Do not mutate tickets.
+ */
+export default function firstUrgentOpenTicket(tickets) {
+  // Write your solution here.
+}
