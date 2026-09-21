d13246011ba16f83740b6f27d364ea99e024d962
Jamal Nezianya <jnezia1@hotmail.com>
2026-09-21 09:14:24 -0500

    Add Array Iteration Deep Dive mastery checkpoint

---
+++ mastery/06-array-iteration-deep-dive/exercises/02-validation.js
@@ -0,0 +1,12 @@
+/**
+ * Return:
+ * {
+ *   hasBlocked: boolean,   // true if ANY task has status "blocked"
+ *   allAssigned: boolean   // true if EVERY task has a non-empty assignee string
+ * }
+ *
+ * Do not mutate tasks.
+ */
+export default function taskValidation(tasks) {
+  // Write your solution here.
+}
