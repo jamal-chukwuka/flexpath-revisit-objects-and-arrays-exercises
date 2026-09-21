26e455d67ecd64c7a7ae7368cb3c31d02879ace3
Jamal Nezianya <jnezia1@hotmail.com>
2026-09-21 09:14:30 -0500

    Add Array Iteration Deep Dive mastery checkpoint

---
+++ mastery/06-array-iteration-deep-dive/exercises/04-index-search.js
@@ -0,0 +1,8 @@
+/**
+ * Return the index of the FIRST reading greater than threshold.
+ * Return -1 when no reading exceeds threshold.
+ * Do not mutate readings.
+ */
+export default function firstReadingAbove(readings, threshold) {
+  // Write your solution here.
+}
