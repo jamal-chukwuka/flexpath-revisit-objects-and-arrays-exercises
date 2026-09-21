705171ba6727799aeca7d24fc1add7af6bd30703
Jamal Nezianya <jnezia1@hotmail.com>
2026-09-21 09:14:32 -0500

    Add Array Iteration Deep Dive mastery checkpoint

---
+++ mastery/06-array-iteration-deep-dive/exercises/05-map-filter.js
@@ -0,0 +1,8 @@
+/**
+ * Return an array of usernames for ACTIVE users whose role equals targetRole.
+ * Preserve input order.
+ * Do not mutate users or return references to a modified input array.
+ */
+export default function activeUsernamesByRole(users, targetRole) {
+  // Write your solution here.
+}
